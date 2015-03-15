1fd078fc28513b333b7305c15fee42ffcb25aa21
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2015-03-15 21:39:25 +0000

    First draft complete

--- ContactTest.java
+++ ContactTest.java
@@ -9,33 +9,55 @@ import static org.junit.Assert.*;
 public class ContactTest {
 
 	/**
-	 * Tests that the returned contact ID matches the ID assigned during instantiation.
+	 * Creates a contact object to be used in all tests.
+	 */
+	@Before
+	public void buildUp() {
+		Contact myContact = new Contact("Bruce Wayne");
+	}
+
+	/**
+	 * Tests getId().
+	 * The returned contact ID should match the ID assigned during instantiation.
 	 */
 	public void shouldReturnCorrectId() {
-		Contact myContact = new Contact();
+		buildUp();
 		int expectedId = 1;
 		int actualId = myContact.getId();
 		assertEquals(expectedId, actualId);
 	}
 
 	/**
-	 * Tests that the returned name matches the name created during instantiation.
+	 * Tests getName().
+	 * The returned name should match the name created during instantiation.
 	 */
 	public void shouldReturnCorrectName() {
-		Contact myContact = new Contact();
+		buildUp();
 		String expectedName = "Bruce Wayne";
 		String actualName = myContact.getName();
 		assertEquals(expectedName, actualName);
 	}
 
 	/**
-	 * Tests that the returned notes matches the notes assigned to the Contact object.
+	 * Tests addNotes() and getNotes().
+	 * The returned notes should match those we added.
 	 */
 	public void shouldReturnCorrectNotes() {
-		Contact myContact = new Contact();
+		buildUp();
+		myContact.addNotes("We better catch that pesky Joker.");
 		String expectedNotes = "We better catch that pesky Joker.";
 		String actualNotes = myContact.getNotes();
 		assertEquals(expectedNotes, actualNotes);
 	}
 
+	/**
+	 * Tests that when a Contact object has a null notes field,
+	 * getNotes() returns an empty String.
+	 */
+	public void shouldReturnEmptyString() {
+		buildUp();
+		String expectedNotes = "";
+		String actualNotes = myContact.getNotes();
+		assertEquals(expectedNotes, actualNotes);
+	}
 }
