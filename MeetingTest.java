eda2bdd27986b861e3928d8439806b3fab43c92d
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2015-03-15 23:09:48 +0000

    Added getDate()

--- MeetingTest.java
+++ MeetingTest.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Before;
+import java.util.GregorianCalendar;
+import java.util.Set;
 
 /**
  * Tests the class MeetingImpl.
@@ -19,7 +21,7 @@ public class MeetingTest {
 	}
 
 	/**
-	 * Tests getId.
+	 * Tests getId().
 	 *
 	 * Should return the expeced Meeting Id.
 	 */
@@ -29,4 +31,17 @@ public class MeetingTest {
 		int actualId = myMeeting.getId();
 		assertEquals(expectedId, actualId);
 	}
+
+	/**
+	 * Tests getDate().
+	 *
+	 * Should return the correct date.
+	 */
+	@Test
+	public void shouldReturnCorrectDate() {
+		Calendar expectedDate = new GregorianCalendar(2015, 10, 10, 10, 0);
+		Calendar actualDate = getDate();
+		assertEquals(expectedDate, actualDate);
+	}
+
 }
