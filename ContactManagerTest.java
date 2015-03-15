856c46fe4992ee11795da9b41f49ab88f8525895
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2015-03-15 22:38:43 +0000

    first commit

---
+++ ContactManagerTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+/**
+ * Tests the class ContactManagerImpl.
+ *
+ * @author Gareth Moore.
+ */
+public class ContactManagerTest {
+
+	/**
+	 * Tests addFutureMeeting
+
+}
