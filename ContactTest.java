8ad350ae3658b10c0254d6d5e041649474b8b37a
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2015-03-15 21:29:53 +0000

    added getNotes test

--- ContactTest.java
+++ ContactTest.java
@@ -11,7 +11,7 @@ public class ContactTest {
 	/**
 	 * Tests that the returned contact ID matches the ID assigned during instantiation.
 	 */
-	public shouldReturnCorrectId() {
+	public void shouldReturnCorrectId() {
 		Contact myContact = new Contact();
 		int expectedId = 1;
 		int actualId = myContact.getId();
@@ -21,10 +21,21 @@ public class ContactTest {
 	/**
 	 * Tests that the returned name matches the name created during instantiation.
 	 */
-	public shouldReturnCorrectName() {
+	public void shouldReturnCorrectName() {
 		Contact myContact = new Contact();
 		String expectedName = "Bruce Wayne";
 		String actualName = myContact.getName();
 		assertEquals(expectedName, actualName);
 	}
+
+	/**
+	 * Tests that the returned notes matches the notes assigned to the Contact object.
+	 */
+	public void shouldReturnCorrectNotes() {
+		Contact myContact = new Contact();
+		String expectedNotes = "We better catch that pesky Joker.";
+		String actualNotes = myContact.getNotes();
+		assertEquals(expectedNotes, actualNotes);
+	}
+
 }
