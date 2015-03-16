1b6287953da2b7a29b23589a4aaf14ca664db12c
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2015-03-16 22:52:34 +0000

    Addded 1 test

--- ContactManagerTest.java
+++ ContactManagerTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Before;
+import java.util.GregorianCalendar;
 
 /**
  * Tests the class ContactManagerImpl.
@@ -8,8 +9,31 @@ import org.junit.Before;
  * @author Gareth Moore.
  */
 public class ContactManagerTest {
+	ContactManager myContactManager; //A ContactManager object to be used in testing.
+	Set<Contact> testSet; // A set of contacts to be used in testing.
 
 	/**
-	 * Tests addFutureMeeting
+	 * Instantiates objects to be used in testing.
+	 */
+	@Before
+	public void buildUp() {
+		testSet.add(new Contact("Bruce Wayne"));
+		testSet.add(new Contact("Clark Kent"));
+		myContactManager = new ContactManagerImpl();
+	}
+
+	/**
+	 * Tests addFutureMeeting().
+	 *
+	 * Should throw IllegalArgumentException when supplied date is in the past.
+	 */
+	public void shouldThrowIllegalArgumentException() {
+		try {
+			myContactManager.addFutureMeeting(testSet, new GregorianCalendar(1900, 1, 1, 10, 15));
+		} catch (IllegalArgumentException ex) {
+			boolean exceptionThrown;
+		}
+		assertTrue(exceptionThrown);
+	}
 
 }
