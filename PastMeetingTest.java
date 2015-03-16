e118c2bd0ffbaf1775bf161306c31cd0a746fb76
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2015-03-16 18:55:38 +0000

    Tests passs with minimal code.

--- PastMeetingTest.java
+++ PastMeetingTest.java
@@ -1,6 +1,10 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Before;
+import java.util.GregorianCalendar;
+import java.util.Calendar;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
  * Tests the class PastMeetingImpl.
@@ -10,13 +14,19 @@ import org.junit.Before;
 public class PastMeetingTest {
 
 	PastMeeting myPastMeeting; //A PastMeeting object for testing purposes.
+	Contact batman, superman; // Contact object for testing.
 
 	/**
-	 * Instantiate a PastMeeting object for testing.
+	 * Instantiates a PastMeeting object for testing.
 	 */
 	@Before
 	public void buildUp() {
-		myPastMeeting = new PastMeetingImpl("Scarecrow has escaped");
+		Set<Contact> tempContactSet = new CopyOnWriteArraySet<Contact>();
+		batman = new ContactImpl("Bruce Wayne");
+		tempContactSet.add(batman);
+		superman = new ContactImpl("Clark Kent");
+		tempContactSet.add(superman);
+		myPastMeeting = new PastMeetingImpl(tempContactSet, new GregorianCalendar(2015, 10, 10, 10, 0), "Scarecrow has escaped");
 	}
 
 	/**
@@ -26,7 +36,7 @@ public class PastMeetingTest {
 	 */
 	@Test
 	public void shouldReturnCorrectPastMeetingNotes() {
-		String expectedNotes = "Scarecrow has excaped";
+		String expectedNotes = "Scarecrow has escaped";
 		String actualNotes = myPastMeeting.getNotes();
 		assertEquals(expectedNotes, actualNotes);
 	}
